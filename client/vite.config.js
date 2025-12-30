c0e75f192c13e44a2115f56ab6f6a693808b6bcd
Chamara Abeyrathna <chamaragithub21@gmail.com>
2025-12-31 02:48:27 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: '/AI_Movie_Hub/'
+  plugins: [react()],
+  base: '/AI_Movie_Hub/', 
 })
